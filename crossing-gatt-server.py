3a0791b08f9387ed0d3a16be03ff609dc2b254ff
Armaan Gera <armaangera@hotmail.com>
2024-12-10 13:23:43 -0500

    changed GATT server file again

--- crossing-gatt-server.py
+++ crossing-gatt-server.py
@@ -3,13 +3,16 @@ import dbus
 import dbus.service
 import dbus.mainloop.glib
 from gi.repository import GLib
-import threading
 
-# UUIDs for the custom service and characteristic
 SERVICE_UUID = '12345678-1234-5678-1234-56789abcdef0'
-CHAR_UUID    = 'abcdef01-1234-5678-1234-56789abcdef0'
+CHAR_UUID = 'abcdef01-1234-5678-1234-56789abcdef0'
+
+# Adapted from BlueZ example-gatt-server to provide a stable, known-working baseline.
 
 class Application(dbus.service.Object):
+    """
+    org.bluez.GattApplication1 interface implementation.
+    """
     PATH_BASE = '/org/bluez/example'
     def __init__(self, bus):
         self.path = self.PATH_BASE
@@ -26,13 +29,17 @@ class Application(dbus.service.Object):
     def get_services(self):
         return self.services
 
-    @dbus.service.method("org.bluez.GattApplication1",
-                         in_signature="", out_signature="")
+    @dbus.service.method('org.bluez.GattApplication1',
+                         in_signature='', out_signature='')
     def Release(self):
-        print("GATT application released")
+        print('GATT application released')
 
 class Service(dbus.service.Object):
+    """
+    org.bluez.GattService1 interface implementation.
+    """
     PATH_BASE = '/org/bluez/example/service'
+
     def __init__(self, index, uuid, primary, bus):
         self.path = self.PATH_BASE + str(index)
         self.bus = bus
@@ -41,29 +48,28 @@ class Service(dbus.service.Object):
         self.characteristics = []
         dbus.service.Object.__init__(self, bus, self.path)
 
-    def get_path(self):
-        return dbus.ObjectPath(self.path)
-
     def add_characteristic(self, characteristic):
         self.characteristics.append(characteristic)
 
     def get_properties(self):
         return {
-            "org.bluez.GattService1": {
-                "UUID": self.uuid,
-                "Primary": self.primary,
+            'org.bluez.GattService1': {
+                'UUID': self.uuid,
+                'Primary': self.primary,
             }
         }
 
-    @dbus.service.method("org.freedesktop.DBus.Properties",
-                         in_signature="s", out_signature="a{sv}")
+    @dbus.service.method('org.freedesktop.DBus.Properties',
+                         in_signature='s', out_signature='a{sv}')
     def GetAll(self, interface):
-        if interface != "org.bluez.GattService1":
-            raise dbus.exceptions.DBusException(
-                "org.freedesktop.DBus.Error.InvalidArgs: Invalid interface")
+        if interface != 'org.bluez.GattService1':
+            raise dbus.exceptions.DBusException('org.freedesktop.DBus.Error.InvalidArgs', 'Invalid interface')
         return self.get_properties()
 
 class Characteristic(dbus.service.Object):
+    """
+    org.bluez.GattCharacteristic1 interface implementation.
+    """
     def __init__(self, uuid, flags, service, bus):
         self.path = service.get_path() + '/char0'
         self.bus = bus
@@ -74,66 +80,65 @@ class Characteristic(dbus.service.Object):
         self.notifying = False
         dbus.service.Object.__init__(self, bus, self.path)
 
-    def get_path(self):
-        return dbus.ObjectPath(self.path)
-
     def get_properties(self):
         return {
-            "org.bluez.GattCharacteristic1": {
-                "UUID": self.uuid,
-                "Service": self.service.get_path(),
-                "Flags": self.flags,
-                "Value": self.value
+            'org.bluez.GattCharacteristic1': {
+                'UUID': self.uuid,
+                'Service': self.service.get_path(),
+                'Flags': self.flags,
+                'Value': self.value
             }
         }
 
-    @dbus.service.method("org.freedesktop.DBus.Properties",
-                         in_signature="s", out_signature="a{sv}")
+    @dbus.service.method('org.freedesktop.DBus.Properties',
+                         in_signature='s', out_signature='a{sv}')
     def GetAll(self, interface):
-        if interface != "org.bluez.GattCharacteristic1":
-            raise dbus.exceptions.DBusException(
-                "org.freedesktop.DBus.Error.InvalidArgs: Invalid interface")
+        if interface != 'org.bluez.GattCharacteristic1':
+            raise dbus.exceptions.DBusException('org.freedesktop.DBus.Error.InvalidArgs', 'Invalid interface')
         return self.get_properties()
 
-    @dbus.service.method("org.bluez.GattCharacteristic1",
-                         in_signature="", out_signature="ay")
+    @dbus.service.method('org.bluez.GattCharacteristic1',
+                         in_signature='', out_signature='ay')
     def ReadValue(self):
         return dbus.ByteArray(self.value)
 
-    @dbus.service.method("org.bluez.GattCharacteristic1",
-                         in_signature="", out_signature="")
+    @dbus.service.method('org.bluez.GattCharacteristic1',
+                         in_signature='', out_signature='')
     def StartNotify(self):
         if self.notifying:
             return
         self.notifying = True
-        self.add_timeout()
+        self._add_timeout()
 
-    def add_timeout(self):
+    def _add_timeout(self):
         if self.notifying:
-            GLib.timeout_add(1000, self.notify_cb)
+            # Just send periodic notifications as a heartbeat
+            GLib.timeout_add(2000, self._notify_cb)
 
-    def notify_cb(self):
+    def _notify_cb(self):
         if not self.notifying:
             return False
-        self.PropertiesChanged("org.bluez.GattCharacteristic1",
-                               {"Value": dbus.ByteArray(self.value)}, [])
+        self.PropertiesChanged('org.bluez.GattCharacteristic1',
+                               {'Value': dbus.ByteArray(self.value)},
+                               [])
         return True
 
-    @dbus.service.method("org.bluez.GattCharacteristic1",
-                         in_signature="", out_signature="")
+    @dbus.service.method('org.bluez.GattCharacteristic1',
+                         in_signature='', out_signature='')
     def StopNotify(self):
         self.notifying = False
 
-    @dbus.service.signal("org.freedesktop.DBus.Properties",
-                         signature="sa{sv}as")
+    @dbus.service.signal('org.freedesktop.DBus.Properties',
+                         signature='sa{sv}as')
     def PropertiesChanged(self, interface, changed, invalidated):
         pass
 
     def set_crossing_state(self, state_str):
         self.value = bytearray(state_str, 'utf-8')
         if self.notifying:
-            self.PropertiesChanged("org.bluez.GattCharacteristic1",
-                                   {"Value": dbus.ByteArray(self.value)}, [])
+            self.PropertiesChanged('org.bluez.GattCharacteristic1',
+                                   {'Value': dbus.ByteArray(self.value)},
+                                   [])
 
 def register_app_cb():
     print("GATT application registered")
@@ -144,20 +149,22 @@ def register_app_error_cb(error):
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 bus = dbus.SystemBus()
-adapter_path = "/org/bluez/hci0"
 
 app = Application(bus)
 service = Service(0, SERVICE_UUID, True, bus)
-char = Characteristic(CHAR_UUID, ["read","notify"], service, bus)
+char = Characteristic(CHAR_UUID, ['read','notify'], service, bus)
 service.add_characteristic(char)
 app.add_service(service)
 
-manager = dbus.Interface(bus.get_object("org.bluez", "/org/bluez"),
-                         "org.bluez.GattManager1")
+manager = dbus.Interface(bus.get_object('org.bluez', '/org/bluez'),
+                         'org.bluez.GattManager1')
 
 mainloop = GLib.MainLoop()
-manager.RegisterApplication(app.get_path(), {},
+
+# Use dbus.Dictionary explicitly with a known signature to avoid ValueError on empty dict
+manager.RegisterApplication(app.get_path(),
+                            dbus.Dictionary({}, signature='sv'),
                             reply_handler=register_app_cb,
                             error_handler=register_app_error_cb)
 
-threading.Thread(target=mainloop.run).start()
+mainloop.run()
